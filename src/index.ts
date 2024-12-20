4426814f7828a54a675b76025710d00179d07fe1
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2024-12-20 19:34:26 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ connectDB()
 app.use(express.json())
 app.use(urlencoded())
 app.use(cors({
-    origin: ["https://secondbrain-d98e.onrender.com", " http://localhost:5173"],
+    origin: ["https://secondbrain-d98e.onrender.com", "http://localhost:5173"],
     credentials: true
 }))
 app.use(cookieParser())
